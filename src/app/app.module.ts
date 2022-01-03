e6a21bb6ed9fa86eca26e3d9c8ee630466d47fc3
enzookita <enzookita@hotmail.com>
2022-01-03 16:12:16 -0400

    compradores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AnimallistarComponent } from './animallistar/animallistar.component';
 import { AnimalincluirComponent } from './animalincluir/animalincluir.component'
 import { FormsModule } from '@angular/forms';
+import { CompradorlistarComponent } from './compradorlistar/compradorlistar.component';
+import { CompradorincluirComponent } from './compradorincluir/compradorincluir.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimallistarComponent,
-    AnimalincluirComponent
+    AnimalincluirComponent,
+    CompradorlistarComponent,
+    CompradorincluirComponent
   ],
   imports: [
     BrowserModule,

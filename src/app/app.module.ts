5eefc90dfb3c8cfed3670a6a141ce3d5c39e36bb
ViniciusPaulaSouza <viniciuspaulasouza@gmail.com>
2022-01-03 17:02:41 -0400

    Leilao incluir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AnimallistarComponent } from './animallistar/animallistar.component';
 import { AnimalincluirComponent } from './animalincluir/animalincluir.component'
 import { FormsModule } from '@angular/forms';
-import { LancelistarComponent } from './lancelistar/lancelistar.component';
-import { VeterinariolistarComponent } from './veterinariolistar/veterinariolistar.component';
-import { VeterinarioincluirComponent } from './veterinarioincluir/veterinarioincluir.component';
 import { LeilaolistarComponent } from './leilaolistar/leilaolistar.component';
-<<<<<<< HEAD
-import { LeilaoincluirComponent } from './leilaoincluir/leilaoincluir.component';
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 167b8f4a52d78aa760603651b0649645e2e63a7f
 import { VendedorlistarComponent } from './vendedorlistar/vendedorlistar.component';
 import { CompradorlistarComponent } from './compradorlistar/compradorlistar.component';
 import { CompradorincluirComponent } from './compradorincluir/compradorincluir.component';
+import { VeterinarioincluirComponent } from './veterinarioincluir/veterinarioincluir.component';
+import { LeilaoincluirComponent } from './leilaoincluir/leilaoincluir.component';
 import { VendedorincluirComponent } from './vendedorincluir/vendedorincluir.component';
+import { VeterinariolistarComponent } from './veterinariolistar/veterinariolistar.component';
 
 
 
@@ -28,31 +23,16 @@ import { VendedorincluirComponent } from './vendedorincluir/vendedorincluir.comp
     AppComponent,
     AnimallistarComponent,
     AnimalincluirComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    LeilaolistarComponent,
-LeilaoincluirComponent
-=======
-<<<<<<< HEAD
-    VendedorlistarComponent
-=======
-=======
-    VendedorlistarComponent,
->>>>>>> 167b8f4a52d78aa760603651b0649645e2e63a7f
-    CompradorlistarComponent,
-    CompradorincluirComponent,
-    LeilaolistarComponent,
-    VeterinarioincluirComponent,
-    VeterinariolistarComponent,
-    LancelistarComponent,
-    VeterinariolistarComponent,
-    VeterinarioincluirComponent,
-    LeilaolistarComponent,
-    VendedorlistarComponent,
-    CompradorlistarComponent,
-    CompradorincluirComponent,
-    VendedorincluirComponent
 
+    LeilaolistarComponent,
+
+    VendedorlistarComponent,
+    CompradorlistarComponent,
+    CompradorincluirComponent,
+    VeterinarioincluirComponent,
+    LeilaoincluirComponent,
+    VendedorincluirComponent,
+    VeterinariolistarComponent
 
   ],
   imports: [

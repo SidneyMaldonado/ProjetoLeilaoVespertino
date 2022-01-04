3caf411cdad007b61d8a25d074007431856629aa
narciza-fialho <narciza.fialho@hotmail.com>
2022-01-04 16:38:37 -0400

    Vendedor excluir

--- src/app/vendedorexcluir/vendedorexcluir.component.ts
+++ src/app/vendedorexcluir/vendedorexcluir.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Mensagem } from '../models/mensagem';
+import { Vendedor } from '../models/vendedor';
+import { VendedorService } from '../services/vendedor.service';
 
 @Component({
   selector: 'app-vendedorexcluir',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendedorexcluir.component.css']
 })
 export class VendedorexcluirComponent implements OnInit {
-
-  constructor() { }
+  mensagem: Mensagem = {
+    mensagem: '',
+    erro: []
+  }
+  vendedor: Vendedor = {
+    id: 0,
+    nome: '',
+    email: '',
+    telefone: '',
+    ativo: false
+  }
+  constructor(private servicoVendedor: VendedorService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get("id")
+    this.servicoVendedor.buscar(id+"").subscribe(dados => this.vendedor = dados)
+  }
+
+  excluir(frm: NgForm) {
+    let id = this.route.snapshot.paramMap.get("id")
+    this.servicoVendedor.excluir(id+"").subscribe(
+      dados => {
+        this.mensagem = dados
+        if (this.mensagem.erro.length == 0) {
+          alert(this.mensagem.mensagem)
+          this.router.navigateByUrl("vendedor")
+        }
+      }
+    )
   }
 
 }

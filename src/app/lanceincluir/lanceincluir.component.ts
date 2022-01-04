e72ef3d01fc94e845e9c86986e63fec6e82e66d2
willianssf <willian.silva552@gmail.com>
2022-01-04 13:37:14 -0400

    lanceincluir pronto

--- src/app/lanceincluir/lanceincluir.component.ts
+++ src/app/lanceincluir/lanceincluir.component.ts
@@ -2,10 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Animal } from '../models/animal';
+import { Comprador } from '../models/comprador';
 import { Lance } from '../models/lance';
+import { Leilao } from '../models/leilao';
 import { Mensagem } from '../models/mensagem';
 import { AnimalService } from '../services/animal.service';
+import { CompradorService } from '../services/comprador.service';
 import { LanceService } from '../services/lance.service';
+import { LeilaoService } from '../services/leilao.service';
 
 @Component({
   selector: 'app-lanceincluir',
@@ -16,14 +20,22 @@ export class LanceincluirComponent implements OnInit {
 
   mensagem: Mensagem = {mensagem:"", erro: [] }
 
-  lance: Lance = {id: 0, data:new Date , valor: 0, ativo: false, idComprador: 0, idLeilao: 0, idAnimal: 0}
+  lance: Lance = {id: 0, data:new Date , valor: 0, ativo: true, idComprador: 0, idLeilao: 0, idAnimal: 0}
   animais: Animal[] = []
+  leilaos: Leilao[] = []
+  compradors: Comprador[] = []
   
-  constructor(private lanceService: LanceService, private router: Router, private servicoAnimal: AnimalService) { }
+  constructor(private lanceService: LanceService, private router: Router, private servicoAnimal: AnimalService, private servicoLeilao: LeilaoService, private servicoComprador: CompradorService) { }
 
     ngOnInit(): void {
       this.servicoAnimal.listar().subscribe(
         dados => this.animais = dados
+      )
+      this.servicoLeilao.listar().subscribe(
+        dados => this.leilaos = dados
+      )
+      this.servicoComprador.listar().subscribe(
+        dados => this.compradors = dados
       )
     }
 

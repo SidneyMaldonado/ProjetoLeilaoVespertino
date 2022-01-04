20faff6054bf066f087d08ffb16787f30af5a58a
ViniciusPaulaSouza <viniciuspaulasouza@gmail.com>
2022-01-04 16:04:53 -0400

    Leilão alterar implementado

--- src/app/leilaoalterar/leilaoalterar.component.ts
+++ src/app/leilaoalterar/leilaoalterar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Leilao } from '../models/leilao';
+import { Mensagem } from '../models/mensagem';
+import { LeilaoService } from '../services/leilao.service';
 
 @Component({
   selector: 'app-leilaoalterar',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeilaoalterarComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  mensagem:Mensagem={
+    mensagem: '',
+    erro: []
   }
+
+  leiloes : Leilao = {
+    id: 0,
+    data: new Date,
+    nome: '',
+    ativo: false
+  }
+
+  constructor(private servicoLeilao: LeilaoService, private route: ActivatedRoute, private router: Router) { }
+
+ ngOnInit(): void {
+   let id = this.route.snapshot.paramMap.get("id")
+   this.servicoLeilao.buscar(id+"").subscribe(
+     dados => this.leiloes = dados
+   )
+
+ }
+
+ alterar( frm: NgForm){
+
+   this.servicoLeilao.alterar(this.leiloes).subscribe(
+     dados => {this.mensagem = dados
+       if (this.mensagem.erro.length == 0){
+         alert( this.mensagem.mensagem)
+         this.router.navigateByUrl("leilao")
+       } else {
+         this.mensagem.erro.forEach( function (value) { alert(value) })
+       }
+     }
+   )
+
+
+ }
 
 }

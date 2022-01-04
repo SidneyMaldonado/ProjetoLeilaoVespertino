856c3b6751442d419d57041f9019faa4408367e7
Luiz Felipe T. Martins <lmartins@itelinfo.com>
2022-01-04 16:36:19 -0400

    veterinario alterar

--- src/app/veterinarioalterar/veterinarioalterar.component.ts
+++ src/app/veterinarioalterar/veterinarioalterar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Mensagem } from '../models/mensagem';
+import { Veterinario } from '../models/veterinario';
+import { VeterinarioService } from '../services/veterinario.service';
 
 @Component({
   selector: 'app-veterinarioalterar',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VeterinarioalterarComponent implements OnInit {
 
-  constructor() { }
+  mensagem: Mensagem = {
+    mensagem: '',
+    erro: []
+  }
+
+  veterinario: Veterinario = {
+    id: 0,
+    nome: '',
+    email: '',
+    telefone: '',
+    ativo: false
+  }
+
+  constructor(private servicoVeterinario: VeterinarioService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+
+    this.servicoVeterinario.listarVeterinario().subscribe( dados => this.veterinario = dados)
+    
+    let id = this.route.snapshot.paramMap.get("id")
+    this.servicoVeterinario.buscar(id+"").subscribe(
+      dados => this.veterinario = dados
+    )
+  }
+
+  alterar( frm: NgForm){
+
+    this.servicoVeterinario.alterar(this.veterinario).subscribe(
+      dados => {this.mensagem = dados
+        if (this.mensagem.erro.length == 0){
+          alert( this.mensagem.mensagem)
+          this.router.navigateByUrl("animal")
+        } else {
+          this.mensagem.erro.forEach( function (value) { alert(value) })
+        }
+      }
+    )
+
+
   }
 
 }

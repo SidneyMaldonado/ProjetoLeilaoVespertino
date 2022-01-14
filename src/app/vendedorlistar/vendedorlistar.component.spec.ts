278ccf7a0319e2b6036a40536e9fe6375c13405e
ViniciusPaulaSouza <viniciuspaulasouza@gmail.com>
2022-01-14 14:40:29 -0400

    vendedor listar teste

--- src/app/vendedorlistar/vendedorlistar.component.spec.ts
+++ src/app/vendedorlistar/vendedorlistar.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AnimalService } from '../services/animal.service';
 
 import { VendedorlistarComponent } from './vendedorlistar.component';
 
@@ -8,7 +10,9 @@ describe('VendedorlistarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VendedorlistarComponent ]
+      declarations: [ VendedorlistarComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [AnimalService]
     })
     .compileComponents();
   });
@@ -22,4 +26,21 @@ describe('VendedorlistarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('O titulo deve aparecer',() => {
+    const titulo = fixture.debugElement.nativeElement.querySelector("#title");
+    let expected: string = "Lista de Vendedores";
+    let result: string = titulo.innerHTML;
+    expect(result).toEqual(expected)
+  });
+
+  it('O rodape deve aparecer',() => {
+    const rodape = fixture.debugElement.nativeElement.querySelector("#rodape");
+    let expected: string = "todos os direitos reservados"
+    let result = rodape.innerHTML;
+    console.log(rodape.innerHTML)
+    expect(result).toEqual(expected);
+  })
+
+
 });

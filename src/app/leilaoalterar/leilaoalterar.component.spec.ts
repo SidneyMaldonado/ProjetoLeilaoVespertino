261b964f9d516b72bc8773658a27d31ee96bd177
enzookita <enzookita@hotmail.com>
2022-01-14 15:08:44 -0400

    leilaoTeste

--- src/app/leilaoalterar/leilaoalterar.component.spec.ts
+++ src/app/leilaoalterar/leilaoalterar.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgForm } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LeilaoService } from '../services/leilao.service';
 
 import { LeilaoalterarComponent } from './leilaoalterar.component';
 
@@ -8,7 +12,9 @@ describe('LeilaoalterarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LeilaoalterarComponent ]
+      declarations: [ LeilaoalterarComponent, NgForm ],
+      imports: [HttpClientTestingModule,RouterTestingModule],
+      providers:[LeilaoService]
     })
     .compileComponents();
   });
@@ -21,5 +27,20 @@ describe('LeilaoalterarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('Titulo renderizado',()=>{
+    const titulo = fixture.debugElement.nativeElement.querySelector("#title");
+    let expected: string = "Alterar Leilao";
+    let result: string = titulo.innerHTML;
+    expect(result).toEqual(expected);
+
+  });
+  it('Botao Salvar', () => {
+
+    const botao = fixture.debugElement.nativeElement.querySelector("#alterar");
+    let expected: string ="Enviar";
+    let result: string = botao.innerHTML;
+    expect(result).toEqual(expected);
+
   });
 });

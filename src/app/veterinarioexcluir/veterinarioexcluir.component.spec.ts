31f4abacd866e04e30f241a75299d7ee894d5817
Luiz Felipe T. Martins <lmartins@itelinfo.com>
2022-01-14 15:01:46 -0400

    Veterinario tests components

--- src/app/veterinarioexcluir/veterinarioexcluir.component.spec.ts
+++ src/app/veterinarioexcluir/veterinarioexcluir.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgForm } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { VeterinarioService } from '../services/veterinario.service';
 
 import { VeterinarioexcluirComponent } from './veterinarioexcluir.component';
 
@@ -8,7 +12,9 @@ describe('VeterinarioexcluirComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VeterinarioexcluirComponent ]
+      declarations: [ VeterinarioexcluirComponent, NgForm ],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [VeterinarioService]
     })
     .compileComponents();
   });
@@ -22,4 +28,22 @@ describe('VeterinarioexcluirComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Titulo renderizado', () =>{
+    const titulo = fixture.debugElement.nativeElement.querySelector("#title");
+    let expected : string = "Veterinário excluir";
+    let result = titulo.innerHTML;
+    expect(result).toEqual(expected);
+  });
+
+  it('Botao excluir', ()=>{
+    const botao = fixture.debugElement.nativeElement.querySelector("#excluir");
+    let expected: string = "Confirmar Exclusão";
+    let result = botao.innerHTML;
+    expect(result).toEqual(expected);
+  })
+
 });
+
+
+
